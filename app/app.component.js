92107088f9eba7745bd07d2600a1d402cf71953f
wbeimar <alejo1002@gmail.com>
2017-06-17 10:04:38 -0500

    Enlaza el template de la lista de productos para que se muestre en la vista

--- app/app.component.js
+++ app/app.component.js
@@ -9,24 +9,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 var AppComponent = (function () {
     function AppComponent() {
-        this.pageTitle = "My Angular 2 Learn";
-        this.firstParagraph = "Lorem ipsum dolor sit amet, consectetur" +
-            "adipiscing elit. Aenean rutrum, ligula id fermentum hendrerit, tellus " +
-            "tellus iaculis sem, ut vehicula leo mi at nulla. Quisque sed ex suscipit," +
-            " consectetur leo vitae, sollicitudin felis. Pellentesque aliquam massa a est " +
-            "malesuada, ac posuere magna lacinia. Suspendisse quis tempor eros. Etiam " +
-            "luctus molestie massa eget ultricies. Donec congue eros elit, at aliquet " +
-            "turpis facilisis vitae. Suspendisse potenti. Nam nec sapien nisl. Vestibulum " +
-            "ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;" +
-            " Curabitur elementum mollis ipsum vel rhoncus. Nam et auctor est. Donec " +
-            "efficitur feugiat nibh ut egestas.";
+        this.pageTitle = "Products Management";
     }
     return AppComponent;
 }());
 AppComponent = __decorate([
     core_1.Component({
         selector: 'pm-app',
-        template: "\n        <div><h1>{{pageTitle}}</h1>\n            <div><strong>My Firts Look to components in Angular</strong></div>\n            <br>\n            <p>{{firstParagraph}}</p>\n        </div>\n    "
+        template: "\n        <div><h1>{{pageTitle}}</h1>\n            <pm-products></pm-products>\n        </div>\n    "
     })
 ], AppComponent);
 exports.AppComponent = AppComponent;
--- app/app.component.ts
+++ app/app.component.ts
@@ -4,22 +4,10 @@ import { Component } from '@angular/core';
     selector: 'pm-app',
     template: `
         <div><h1>{{pageTitle}}</h1>
-            <div><strong>My Firts Look to components in Angular</strong></div>
-            <br>
-            <p>{{firstParagraph}}</p>
+            <pm-products></pm-products>
         </div>
     `
 })
 export class AppComponent {
-    pageTitle : string = "My Angular 2 Learn";
-    firstParagraph : string = "Lorem ipsum dolor sit amet, consectetur" +
-    "adipiscing elit. Aenean rutrum, ligula id fermentum hendrerit, tellus " +
-    "tellus iaculis sem, ut vehicula leo mi at nulla. Quisque sed ex suscipit," +
-    " consectetur leo vitae, sollicitudin felis. Pellentesque aliquam massa a est " +
-    "malesuada, ac posuere magna lacinia. Suspendisse quis tempor eros. Etiam " +
-    "luctus molestie massa eget ultricies. Donec congue eros elit, at aliquet " +
-    "turpis facilisis vitae. Suspendisse potenti. Nam nec sapien nisl. Vestibulum " +
-    "ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;" +
-    " Curabitur elementum mollis ipsum vel rhoncus. Nam et auctor est. Donec " +
-    "efficitur feugiat nibh ut egestas.";
+    pageTitle : string = "Products Management";
  }
