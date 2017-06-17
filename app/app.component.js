ccc84a67b3347dc3bb9040d656e11841b639c7d1
wbeimar <alejo1002@gmail.com>
2017-06-17 08:16:08 -0500

    Adiciona otro párrafo en app.component.ts

--- app/app.component.js
+++ app/app.component.js
@@ -20,13 +20,23 @@ var AppComponent = (function () {
             "ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;" +
             " Curabitur elementum mollis ipsum vel rhoncus. Nam et auctor est. Donec " +
             "efficitur feugiat nibh ut egestas.";
+        this.secondParagraph = "Lorem ipsum dolor sit amet, consectetur" +
+            "adipiscing elit. Aenean rutrum, ligula id fermentum hendrerit, tellus " +
+            "tellus iaculis sem, ut vehicula leo mi at nulla. Quisque sed ex suscipit," +
+            " consectetur leo vitae, sollicitudin felis. Pellentesque aliquam massa a est " +
+            "malesuada, ac posuere magna lacinia. Suspendisse quis tempor eros. Etiam " +
+            "luctus molestie massa eget ultricies. Donec congue eros elit, at aliquet " +
+            "turpis facilisis vitae. Suspendisse potenti. Nam nec sapien nisl. Vestibulum " +
+            "ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;" +
+            " Curabitur elementum mollis ipsum vel rhoncus. Nam et auctor est. Donec " +
+            "efficitur feugiat nibh ut egestas.";
     }
     return AppComponent;
 }());
 AppComponent = __decorate([
     core_1.Component({
         selector: 'pm-app',
-        template: "\n        <div><h1>{{pageTitle}}</h1>\n            <div><strong>My Firts Look to components in Angular</strong></div>\n            <br>\n            <p>{{firstParagraph}}</p>\n        </div>\n    "
+        template: "\n        <div><h1>{{pageTitle}}</h1>\n            <div><strong>My Firts Look to components in Angular</strong></div>\n            <br>\n            <p>{{firstParagraph}}</p>\n            <br>\n            <p>{{secondParagraph}}</p>\n        </div>\n    "
     })
 ], AppComponent);
 exports.AppComponent = AppComponent;
--- app/app.component.ts
+++ app/app.component.ts
@@ -7,12 +7,25 @@ import { Component } from '@angular/core';
             <div><strong>My Firts Look to components in Angular</strong></div>
             <br>
             <p>{{firstParagraph}}</p>
+            <br>
+            <p>{{secondParagraph}}</p>
         </div>
     `
 })
 export class AppComponent {
     pageTitle : string = "My Angular 2 Learn";
     firstParagraph : string = "Lorem ipsum dolor sit amet, consectetur" +
+    "adipiscing elit. Aenean rutrum, ligula id fermentum hendrerit, tellus " +
+    "tellus iaculis sem, ut vehicula leo mi at nulla. Quisque sed ex suscipit," +
+    " consectetur leo vitae, sollicitudin felis. Pellentesque aliquam massa a est " +
+    "malesuada, ac posuere magna lacinia. Suspendisse quis tempor eros. Etiam " +
+    "luctus molestie massa eget ultricies. Donec congue eros elit, at aliquet " +
+    "turpis facilisis vitae. Suspendisse potenti. Nam nec sapien nisl. Vestibulum " +
+    "ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;" +
+    " Curabitur elementum mollis ipsum vel rhoncus. Nam et auctor est. Donec " +
+    "efficitur feugiat nibh ut egestas.";
+    
+    secondParagraph : string = "Lorem ipsum dolor sit amet, consectetur" +
     "adipiscing elit. Aenean rutrum, ligula id fermentum hendrerit, tellus " +
     "tellus iaculis sem, ut vehicula leo mi at nulla. Quisque sed ex suscipit," +
     " consectetur leo vitae, sollicitudin felis. Pellentesque aliquam massa a est " +
