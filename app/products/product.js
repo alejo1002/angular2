5384284c44d052c6b668fa63479aca001748656e
wbeimar <alejo1002@gmail.com>
2017-06-19 14:39:38 -0500

    Crea interfaz product, la importa en el componente de ProductList y la usa como tipo en el arreglo de productos.

---
+++ app/products/product.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/*Se puede especificar una calse que implemente la interface
+  y que agregre funcionalidad en sus métodos*/
+/*export class Product implements IProduct{
+      constructor(
+        public productId : number,
+        public productName: string,
+        public productCode: string,
+        public releaseDate: string,
+        public price: number,
+        public description: string,
+        public starRating: number,
+        public imageUrl: string
+      ){}
+
+      calculateDiscount(percent: number) number{
+          return this.price -(this.price * percent / 100);
+      }
+  }*/ 
+//# sourceMappingURL=product.js.map
